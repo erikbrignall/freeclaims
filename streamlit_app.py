f428fce21673c593ff62d3950a8d9fb5364aa875
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-09-27 12:32:22 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,7 +26,7 @@ if submit_button:
         apikey = st.secrets["apikey"]
         request_data = {"input": query_text, "apikey": apikey}
 
-        with st.spinner('Cogs a whirrin...'):
+        with st.spinner('Analysing Claims Data...'):
             start_time = time.time()
             response = requests.post(url, json=request_data)
             end_time = time.time()
@@ -39,3 +39,10 @@ if submit_button:
             st.write('Response Time:')
             st.write(response_time)
             #print(response.status_code)
+st.header('Free claims examples')
+st.subheader('Account 1: Minor Prang')
+st.write('While driving on High Street, I was stopped at a red light. The car behind me, a blue Ford Fiesta, failed to stop in time and bumped into the rear of my vehicle. The impact was minor, resulting in a small dent on my rear bumper. Both drivers exchanged insurance details, and no injuries were reported.')
+st.subheader('Account 2: Multi-Vehicle Collision')
+st.write('At approximately 8:30 AM, I was traveling eastbound on the M4 when traffic suddenly slowed down due to an accident ahead. I managed to stop in time, but the car behind me, a white BMW, was unable to stop and collided with my vehicle, pushing me into the car in front, a red Toyota. The impact caused significant damage to the front and rear of my car. Emergency services were called, and I was treated for minor whiplash at the scene. All drivers exchanged insurance details, and the police provided a report number.')
+st.subheader('Account 3: Pedestrian Involved Accident')
+st.write('While driving westbound on Bath Road at around 5:00 PM, a pedestrian suddenly stepped onto the road from between parked cars. Despite braking immediately, I was unable to avoid hitting the pedestrian. The pedestrian sustained a broken leg and was taken to the hospital by ambulance. I remained at the scene and provided a statement to the police. My car’s front bumper and windshield were damaged. The police have recorded the incident, and I have provided my insurance details to the pedestrian.')
