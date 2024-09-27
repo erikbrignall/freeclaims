2a76a49f4af858bef1064d8b609d3466b423d875
erikbrignall <31072339+erikbrignall@users.noreply.github.com>
2024-09-27 13:24:34 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,10 @@ st.title('Streamlit LLM interface demo')
 st.header('Claims data - DEMO')
 st.write('The following is a demo of using streamlit as an interface for chat apps. ')
 
-st.sidebar.header['Field Descriptions']
-st.sidebar.write['Collision Type: Single']
-st.sidebar.write['Vehicles']
+with st.sidebar:
+    st.header('Field Descriptions')
+    st.write('Collision Type: Single')
+    st.write('Vehicles')
 
 #openai.api_key = st.secrets["OpenAIapikey"]
 
